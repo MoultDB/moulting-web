f3a476b99b3545536d9a317f9e99ea5040813259
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2023-07-18 15:08:07 +0200

    Refactor css

--- src/components/data/display-images.js
+++ src/components/data/display-images.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import './display-images.css';
 import {CaretRightFill, StarFill} from 'react-bootstrap-icons';
 import ReactPaginate from 'react-paginate';
 
