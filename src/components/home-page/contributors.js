a76479d5e3c9051fcc60038da171155bf21c2b9d
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2025-03-03 18:10:52 +0100

    Add loading animation

--- src/components/home-page/contributors.js
+++ src/components/home-page/contributors.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import ImageService from '../../services/image.service';
 import './contributors.css';
+import Loader from "../common/Loader";
 
 
 export const getContributorUrl = (login) => {
@@ -10,15 +11,19 @@ export const getContributorUrl = (login) => {
 const Contributors = () => {
     const [contributors, setContributors] = useState([]);
     const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const loadContributors = async () => {
+            setLoading(true);
             try {
                 const data = await ImageService.fetchTopContributors();
                 setContributors(data);
             } catch (error) {
                 console.error('Error fetching contributors:', error);
                 setError('Failed to load contributors. Please try again later.');
+            } finally {
+                setLoading(false);
             }
         };
 
@@ -29,19 +34,25 @@ const Contributors = () => {
         <div className="contributors">
             <h4 className="sb-title">TOP CONTRIBUTORS</h4>
             {error && <p className="error">{error}</p>}
-            {contributors.map((contributor, index) => (
-                <div key={index} className="contr-item">
-                    <a href={getContributorUrl(contributor.login)}>
-                        <img src={contributor.iconUrl} alt={contributor.login} width="70" height="70" />
-                    </a>
-                    <div className="contr-author">
-                        <h6>
-                            <a href={getContributorUrl(contributor.login)}>{contributor.login}</a>
-                        </h6>
-                        <span>{contributor.observationCount} observations</span>
-                    </div>
-                </div>
-            ))}
+            {loading ? (
+                <Loader />
+            ) : (
+                <>
+                    {contributors.map((contributor, index) => (
+                        <div key={index} className="contr-item">
+                            <a href={getContributorUrl(contributor.login)}>
+                                <img src={contributor.iconUrl} alt={contributor.login} width="70" height="70" />
+                            </a>
+                            <div className="contr-author">
+                                <h6>
+                                    <a href={getContributorUrl(contributor.login)}>{contributor.login}</a>
+                                </h6>
+                                <span>{contributor.observationCount} observations</span>
+                            </div>
+                        </div>
+                    ))}
+                </>
+            )}
         </div>
     );
 };
